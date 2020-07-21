0db76e91de19935f73437333e93c44adf1f6439c
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2020-07-21 22:26:27 +0800

    do prettier, disable nodeIntegration

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 /* eslint func-names: 0, no-var: 0, vars-on-top:0, prefer-template: 0, prefer-arrow-callback:0 */
-(function () {
+function init() {
   if (!document.querySelectorAll) {
     return;
   }
 
-  var links = document.querySelectorAll('.pure-menu-link');
+  var links = document.querySelectorAll(".pure-menu-link");
   links = Array.prototype.slice.call(links);
 
   var $ = function (id) {
@@ -14,45 +14,46 @@
     return document.querySelector(selector);
   };
 
-  var formQuery = $('query_form');
-  var inputUrl = $('url');
-  var btnSubmit = $('submit_btn');
-  var divLoading = $('loading_div');
-  var divLyric = $('lyric_div');
-  var textareaLyric = $('lyric_textarea');
+  var formQuery = $("query_form");
+  var inputUrl = $("url");
+  var btnSubmit = $("submit_btn");
+  var divLoading = $("loading_div");
+  var divLyric = $("lyric_div");
+  var textareaLyric = $("lyric_textarea");
 
   var showContent = function (id) {
-    var activeCls = 'content-active';
-    q('.' + activeCls).classList.remove(activeCls);
+    var activeCls = "content-active";
+    q("." + activeCls).classList.remove(activeCls);
     $(id).classList.add(activeCls);
 
-    var selectedCls = 'pure-menu-selected';
-    q('.' + selectedCls).classList.remove(selectedCls);
-    q('.pure-menu-item[data-content-id=' + id + ']').classList.add(selectedCls);
+    var selectedCls = "pure-menu-selected";
+    q("." + selectedCls).classList.remove(selectedCls);
+    q(".pure-menu-item[data-content-id=" + id + "]").classList.add(selectedCls);
   };
   var updateTextareaHeight = function () {
-    textareaLyric.style.height = '100px';
+    textareaLyric.style.height = "100px";
     var height = textareaLyric.scrollHeight;
-    textareaLyric.style.height = height + 20 + 'px';
+    textareaLyric.style.height = height + 20 + "px";
   };
 
   var setErrorMsg = function (msg) {
     if (msg === false) {
-      divLoading.style.display = 'none';
-      divLyric.style.display = 'block';
+      divLoading.style.display = "none";
+      divLyric.style.display = "block";
       return;
     }
 
     divLoading.innerHTML = msg;
-    divLoading.style.display = 'block';
-    divLyric.style.display = 'none';
+    divLoading.style.display = "block";
+    divLyric.style.display = "none";
   };
   var setLoading = function () {
-    setErrorMsg('Loading...');
+    setErrorMsg("Loading...");
     btnSubmit.disabled = true;
   };
   var setError = function () {
-    var errMsg = '<span style="color: red;">Failed to get lyric. Please contact franklai.</span>';
+    var errMsg =
+      '<span style="color: red;">Failed to get lyric. Please contact franklai.</span>';
     setErrorMsg(errMsg);
     btnSubmit.disabled = false;
   };
@@ -69,9 +70,8 @@
     updateTextareaHeight(lyric);
   };
 
-
   links.forEach(function (link) {
-    link.addEventListener('click', function (evt) {
+    link.addEventListener("click", function (evt) {
       evt.preventDefault();
 
       var pn = evt.target.parentNode;
@@ -87,34 +87,34 @@
   var selectLyric = function () {
     textareaLyric.select();
   };
-  $('select').addEventListener('click', function () {
+  $("select").addEventListener("click", function () {
     selectLyric();
   });
   if (
     document.queryCommandSupported &&
-    document.queryCommandSupported('copy')
+    document.queryCommandSupported("copy")
   ) {
-    var msg = q('.copied-msg');
+    var msg = q(".copied-msg");
 
-    $('copy').addEventListener('click', function () {
+    $("copy").addEventListener("click", function () {
       selectLyric();
-      document.execCommand('copy');
+      document.execCommand("copy");
 
-      msg.classList.add('fadeout');
+      msg.classList.add("fadeout");
     });
 
-    msg.addEventListener('transitionend', function () {
-      msg.classList.remove('fadeout');
+    msg.addEventListener("transitionend", function () {
+      msg.classList.remove("fadeout");
     });
   } else {
-    $('copy').disabled = true;
+    $("copy").disabled = true;
   }
-  inputUrl.addEventListener('click', function () {
+  inputUrl.addEventListener("click", function () {
     inputUrl.select();
   });
 
   var doAjaxQuery = function (val) {
-    var url = 'app?url=' + encodeURIComponent(val);
+    var url = "app?url=" + encodeURIComponent(val);
 
     fetch(url)
       .then(function (resp) {
@@ -137,47 +137,34 @@
   };
   var doElectronQuery = function (val) {
     try {
-      const engine = require('lyric-get-engine'); // eslint-disable-line global-require
-      engine
-        .get_full(val)
-        .then(function (lyric) {
-          if (!lyric) {
-            setError();
-            return;
-          }
-
-          setResult(lyric);
-        })
-        .catch(function (reason) {
+      const { ipcRenderer } = require("electron");
+      ipcRenderer.on("asynchronous-reply", (event, lyric) => {
+        if (!lyric) {
           setError();
-          console.log('Error:', reason);
-        });
+          return;
+        }
+
+        setResult(lyric);
+      });
+      ipcRenderer.send("asynchronous-message", val);
     } catch (err) {
-      console.log('Error:', err);
+      console.log("Error:", err);
     }
   };
 
   var doQuery = function () {
     var val = inputUrl.value.trim();
-    if (val === '' || val.toLowerCase().match('https?://') === null) {
+    if (val === "" || val.toLowerCase().match("https?://") === null) {
       return false;
     }
 
     setLoading();
 
-    if (
-      window.process &&
-      window.process.versions &&
-      window.process.versions.electron !== undefined
-    ) {
-      return doElectronQuery(val);
-    }
-
-    return doAjaxQuery(val);
+    return doElectronQuery(val);
   };
 
-  $('examples').addEventListener('click', function (evt) {
-    if (evt.target.tagName.toLowerCase() !== 'a') {
+  $("examples").addEventListener("click", function (evt) {
+    if (evt.target.tagName.toLowerCase() !== "a") {
       return;
     }
     if (evt.metaKey || evt.ctrlKey || evt.shiftKey || evt.altKey) {
@@ -187,12 +174,14 @@
 
     inputUrl.value = evt.target.href;
 
-    showContent('main');
+    showContent("main");
     doQuery();
   });
 
-  formQuery.addEventListener('submit', function (evt) {
+  formQuery.addEventListener("submit", function (evt) {
     evt.preventDefault();
     doQuery();
   });
-}());
+}
+
+document.addEventListener("DOMContentLoaded", init);
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const electron = require('electron');
+const path = require("path");
+const electron = require("electron");
 // Module to control application life.
-const { app, BrowserWindow, shell } = electron;
+const { app, BrowserWindow, shell, ipcMain } = electron;
+const engine = require("lyric-get-engine");
 // Module to create native browser window.
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -13,7 +15,10 @@ function createWindow() {
     width: 1024,
     height: 768,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: path.join(app.getAppPath(), "js/script.js"),
     },
   });
 
@@ -21,17 +26,17 @@ function createWindow() {
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
-  //   mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
+  mainWindow.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
   });
 
-  mainWindow.webContents.on('new-window', (event, url) => {
+  mainWindow.webContents.on("new-window", (event, url) => {
     event.preventDefault();
     shell.openExternal(url);
   });
@@ -40,23 +45,31 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
+});
+
+ipcMain.on("asynchronous-message", (event, arg) => {
+  console.log(arg); // prints "ping"
+
+  engine.get_full(arg).then((lyric) => {
+    event.reply("asynchronous-reply", lyric);
+  });
 });
 
 // In this file you can include the rest of your app's specific main process
